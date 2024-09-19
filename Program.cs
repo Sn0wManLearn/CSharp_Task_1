fef6686cae92e8fe7a4af9c7f97731544df331b2
Bl1nker <Alex_001@sibmail.com>
2024-09-19 11:20:55 +0700

    Solve two problems with a probably null string

--- Program.cs
+++ Program.cs
@@ -1,47 +1,59 @@
 ﻿using System;
 
-class MyClass 
+class MyClass
 {
-    static void Main() 
+    static void Main()
     {
-        string input = Console.ReadLine();
+        string? input = Console.ReadLine();
         // Убираем все пробелы в полученной строке
-        input = input.Replace(" ", "");
+        if (input != null) input = input.Replace(" ", "");
+        else
+        {
+            Console.WriteLine("Введена пустая строка.");
+            return;
+        }
         // Метод TryParse не может перобразовать string с числом с точкой в decimal, если число написано через точку, заменяем на запятую.
-        if(input.Contains('.')) input = input.Replace('.', ','); 
+        if (input.Contains('.')) input = input.Replace('.', ',');
 
         // Поиск оператора действия, его индекс будет опеределять где заканчивается одно число и начинается другое
         int idx;
-        char op;        
-        if(input.Contains('+')) {
+        char op;
+        if (input.Contains('+'))
+        {
             idx = input.IndexOf('+');
             op = '+';
-        }        
-        else if(input.Contains('/')) {
+        }
+        else if (input.Contains('/'))
+        {
             idx = input.IndexOf('/');
             op = '/';
         }
-        else if(input.Contains('*')) {
+        else if (input.Contains('*'))
+        {
             idx = input.IndexOf('*');
             op = '*';
         }
-        else {
+        else
+        {
             idx = input.IndexOf('-', 1);
             op = '-';
         }
-        
+
         // Прерывание программы, если разделитель не найден
-        if(idx == -1) {
+        if (idx == -1)
+        {
             Console.WriteLine("Данные введены неверно.\nПроверьте правильность введенных данных");
             return;
         }
 
         // Преобразование частей исходной строки в числа, с проверкой корректности ввода. Прерывание выполнения, в случае ошибки ввода
-        if(!Decimal.TryParse(input[..idx], out decimal firstNum)) {
+        if (!Decimal.TryParse(input[..idx], out decimal firstNum))
+        {
             Console.WriteLine("Первое число введено неверно");
             return;
         }
-        if(!Decimal.TryParse(input[(idx + 1)..], out decimal secondNum)) {
+        if (!Decimal.TryParse(input[(idx + 1)..], out decimal secondNum))
+        {
             Console.WriteLine("Второе число введено неверно");
             return;
         }
