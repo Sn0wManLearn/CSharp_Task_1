fb9b09550d63636b8c73977c0b445558ef20053c
Bl1nker <Alex_001@sibmail.com>
2024-09-19 13:59:46 +0700

    add some updates

--- Program.cs
+++ Program.cs
@@ -4,40 +4,24 @@ class MyClass
 {
     static void Main()
     {
+        Console.WriteLine("Введите выражение, в формате а + b");
         string? input = Console.ReadLine();
-        // Убираем все пробелы в полученной строке
-        if (input != null) input = input.Replace(" ", "");
-        else
+        
+        if (input == null) 
         {
             Console.WriteLine("Введена пустая строка.");
             return;
         }
-        // Метод TryParse не может перобразовать string с числом с точкой в decimal, если число написано через точку, заменяем на запятую.
+
+        // Убираем все пробелы в полученной строке
+        input = input.Replace(" ", "");
+
+        // Метод TryParse не может перобразовать string с числом в котором десятичная часть выделена точкой в decimal, 
+        // если число написано через точку, заменяем на запятую.
         if (input.Contains('.')) input = input.Replace('.', ',');
 
         // Поиск оператора действия, его индекс будет опеределять где заканчивается одно число и начинается другое
-        int idx;
-        char op;
-        if (input.Contains('+'))
-        {
-            idx = input.IndexOf('+');
-            op = '+';
-        }
-        else if (input.Contains('/'))
-        {
-            idx = input.IndexOf('/');
-            op = '/';
-        }
-        else if (input.Contains('*'))
-        {
-            idx = input.IndexOf('*');
-            op = '*';
-        }
-        else
-        {
-            idx = input.IndexOf('-', 1);
-            op = '-';
-        }
+        char op = FindOperand(input, out int idx);
 
         // Прерывание программы, если разделитель не найден
         if (idx == -1)
@@ -45,7 +29,6 @@ class MyClass
             Console.WriteLine("Данные введены неверно.\nПроверьте правильность введенных данных");
             return;
         }
-
         // Преобразование частей исходной строки в числа, с проверкой корректности ввода. Прерывание выполнения, в случае ошибки ввода
         if (!Decimal.TryParse(input[..idx], out decimal firstNum))
         {
@@ -65,10 +48,36 @@ class MyClass
             '-' => firstNum - secondNum,
             '/' => firstNum / secondNum,
             '*' => firstNum * secondNum,
-            _ => 0,
+            _ => 0
         };
 
         // Вывод результата на консоль
-        Console.WriteLine(result);
+        Console.WriteLine($"{firstNum} {op} {secondNum} = {result}");
+    }
+    static char FindOperand(string str, out int idx)
+    {
+        char op;
+        if (str.Contains('+'))
+        {
+            idx = str.IndexOf('+');
+            op = '+';
+        }
+        else if (str.Contains('/'))
+        {
+            idx = str.IndexOf('/');
+            op = '/';
+        }
+        else if (str.Contains('*'))
+        {
+            idx = str.IndexOf('*');
+            op = '*';
+        }
+        else
+        {
+            idx = str.IndexOf('-', 1);
+            op = '-';
+        }
+
+        return op;
     }
 }
